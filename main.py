2eb101be15c9837007ad89608de41db216c0a49e
camiel <camiel.co@pixel-nexus.com>
2022-07-07 16:03:58 +0200

    rewrite main

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
+"""Run the IAA study over data provided in `assets/iaa_set_dnaf.zip`.
+
+Args:
+    out_dir (Path): All study results will be written to this dir.
+    restricted_mode (bool): If True, heads belonging to modifiers will not be considered during head matching.
+    most_specific (bool): If True, the most specific annotated IPTC codes will be used for IPTC match evaluation. If False, the codes will be generalized to their top-level equivalents.
+"""
+from datetime import datetime
+import logging
 from pathlib import Path
-from iaastudy.match_fns import match_fns
+
 from iaastudy.iaastudy import run_iaa_study
+from iaastudy.match_fns import match_fns
 
 
-def main(
-    out_dir: Path,
-    restricted: bool = False,
-    specific_topics: bool = False,
-):
-    """Run the IAA study over data provided in `assets/iaa_set_dnaf.zip`.
+data_zip = Path("assets/iaa_set_dnaf.zip")
+timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+out_dir = Path("output") / timestamp
+out_dir.mkdir()
 
-    Args:
-        out_dir (Path): All study results will be written to this dir.
-        restricted (bool): If True, heads belonging to modifiers will not be considered during head matching.
-        specific_topics (bool): If True, the most specific annotated IPTC codes will be used for IPTC match evaluation. If False, the codes will be generalized to their top-level equivalents.
-    """
+logging.basicConfig(filename=out_dir/"log.log", level=logging.DEBUG)
 
-    data_zip = Path("assets/iaa_set_dnaf.zip")
+# Run with restricted heads.
+d = out_dir / "restricted_heads"
+d.mkdir(parents=True)
+run_iaa_study(
+    data_zip,
+    d,
+    match_fns["fallback"],
+    restricted_mode=True,
+    most_specific=False,
+)
 
-    run_iaa_study(
-        data_zip, out_dir, match_fns["fallback"], restricted, specific_topics
-    )
-
-
-if __name__ == "__main__":
-    out_dir = Path("data/results")
-    main(out_dir / "restricted_heads", restricted=True)
-    main(out_dir / "all_heads", restricted=False)
+# Run with all heads.
+d = out_dir / "all_heads"
+d.mkdir(parents=True)
+run_iaa_study(
+    data_zip,
+    d,
+    match_fns["fallback"],
+    restricted_mode=False,
+    most_specific=False,
+)
